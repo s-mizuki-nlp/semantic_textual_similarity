da6017f0482b0124c5e960fc2e1c65276c337b66
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-04-29 18:12:49 +0900

    fix: changed default encoding from ascii to utf8

--- common/loader/text.py
+++ common/loader/text.py
@@ -8,9 +8,10 @@ class AbstractLoader(object):
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, file_path, n_minibatch=1):
+    def __init__(self, file_path, n_minibatch=1, encoding="utf8"):
         self._path = file_path
         self._n_mb = n_minibatch
+        self._encoding = encoding
         self._n_rows = None
 
     @abstractmethod
@@ -26,7 +27,7 @@ class AbstractLoader(object):
             return self._n_rows
 
         ret = 0
-        with io.open(self._path, mode="r") as ifs:
+        with io.open(self._path, mode="r", encoding=self._encoding) as ifs:
             for _ in ifs:
                 ret += 1
         self._n_rows = ret
@@ -34,23 +35,23 @@ class AbstractLoader(object):
 
 class TextLoader(AbstractLoader):
 
-    def __init__(self, file_path, strip=None):
-        super(TextLoader, self).__init__(file_path, n_minibatch=0)
+    def __init__(self, file_path, strip=None, encoding="utf8"):
+        super(TextLoader, self).__init__(file_path, n_minibatch=0, encoding=encoding)
         self._strip = strip
 
     def __iter__(self):
-        with io.open(self._path, mode="r") as ifs:
+        with io.open(self._path, mode="r", encoding=self._encoding) as ifs:
             for line in ifs:
                 yield line.strip(self._strip)
 
 class MinibatchTextLoader(AbstractLoader):
 
-    def __init__(self, file_path, n_minibatch, strip=None):
-        super(MinibatchTextLoader, self).__init__(file_path, n_minibatch)
+    def __init__(self, file_path, n_minibatch, strip=None, encoding="utf8"):
+        super(MinibatchTextLoader, self).__init__(file_path, n_minibatch, encoding)
         self._strip = strip
 
     def __iter__(self):
-        with io.open(self._path, mode="r") as ifs:
+        with io.open(self._path, mode="r", encoding=self._encoding) as ifs:
             lst_ret = []
             for line in ifs:
                 lst_ret.append(line.strip(self._strip))
